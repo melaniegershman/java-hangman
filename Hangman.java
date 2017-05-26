5ae7b930fd96fc29d88756215671013cf20c4205
Melanie Gershman <melanie.gershman@gmail.com>
2017-05-26 10:49:51 -0400

    Initial commit

---
+++ Hangman.java
@@ -0,0 +1,14 @@
+public class Hangman {
+
+  public static void main(String[] args) {
+    // Your incredible code goes here...
+    Game game = new Game("treehouse");
+    Prompter prompter = new Prompter(game);
+
+    while (game.getRemainingTries() > 0 && !game.isWon()) {
+      prompter.displayProgress();
+      prompter.promptForGuess();
+    }
+    prompter.displayOutcome();
+  }
+}
